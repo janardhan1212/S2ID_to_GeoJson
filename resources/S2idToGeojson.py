942d9c8cbf9c4439b773000a21464aaf811a3984
Janardhan <janardhan.y@go-jek.com>
2020-09-22 20:18:04 +0530

    change request data type

--- resources/S2idToGeojson.py
+++ resources/S2idToGeojson.py
@@ -1,15 +1,17 @@
+import json
+
 import geojson
 import s2sphere
-from flask import request
+from flask import jsonify, request
 from flask_restful import Resource
 
 
 class S2idToGeojson(Resource):
-  def get(self):
-    ids = request.form.get('ids')
-    convertedIds = ids.split(',') 
-
-    result = self.s22g(convertedIds)
+  def post(self):
+    decoded_response=request.data.decode("utf-8")
+    parsed_data = json.loads(decoded_response)
+    s2ids = parsed_data['s2ids']
+    result = self.s22g(s2ids)
     return result, 200
 
   def get_feature(self, s):
@@ -20,9 +22,9 @@ class S2idToGeojson(Resource):
     g = geojson.Polygon(x)
     f = geojson.Feature(geometry=g, properties={'s2id':str(s), 'lvl': str(c.level())})
     return f
-  def s22g(self,s):
-    s2 = [int(x) for x in s]
-
+  def s22g(self,s2ids):
+    s2 = [int(x) for x in s2ids]
+    print(s2)
     lf = [self.get_feature(s) for s in s2]
     fc = geojson.FeatureCollection(lf)
     return fc
